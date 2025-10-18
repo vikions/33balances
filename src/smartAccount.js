b3ea469b8b3f4f46701366145ad30c3d51b8afa7
vikions <plambir1994@mail.ru>
2025-10-18 22:37:54 +0700

    49

--- src/smartAccount.js
+++ src/smartAccount.js
@@ -9,14 +9,14 @@ import {
   createBundlerClient,
   createPaymasterClient,
 } from "viem/account-abstraction";
-import { monadTestnet, ENTRY_POINT_V06 } from "./chain";
+import { monadTestnet } from "./chain"; 
 import {
   Implementation,
   toMetaMaskSmartAccount,
 } from "@metamask/delegation-toolkit";
 import { getEip1193Provider } from "./fcProvider";
 
-// === ENV ===
+
 const RPC = import.meta.env.VITE_MONAD_RPC;
 const PIMLICO_CHAIN = import.meta.env.VITE_PIMLICO_CHAIN || "monad-testnet";
 const PIMLICO_API_KEY = import.meta.env.VITE_PIMLICO_API_KEY;
@@ -26,6 +26,10 @@ const BUNDLER_URL =
 
 if (!PIMLICO_API_KEY) throw new Error("Missing VITE_PIMLICO_API_KEY");
 
+
+export const ENTRY_POINT_V07 =
+  "0x0000000071727De22E5E9d8BAf0edAc6f37da032";
+
 export function makePublicClient() {
   return createPublicClient({ chain: monadTestnet, transport: http(RPC) });
 }
@@ -33,7 +37,7 @@ export function makePublicClient() {
 export async function initSmartAccount() {
   const eip1193 = await getEip1193Provider();
 
-  // создаём временный клиент для получения EOA
+ 
   const tmpClient = createWalletClient({
     chain: monadTestnet,
     transport: custom(eip1193),
@@ -48,7 +52,7 @@ export async function initSmartAccount() {
 
   const publicClient = makePublicClient();
 
-  // === создаём MetaMask Smart Account ===
+  
   const smartAccount = await toMetaMaskSmartAccount({
     client: publicClient,
     implementation: Implementation.Hybrid,
@@ -58,14 +62,14 @@ export async function initSmartAccount() {
     chain: monadTestnet,
   });
 
-  // === Pimlico Bundler ===
+  
   const bundler = createBundlerClient({
     client: publicClient,
-    entryPoint: ENTRY_POINT_V06,
+    entryPoint: ENTRY_POINT_V07, 
     transport: http(BUNDLER_URL),
   });
 
-  // === Pimlico Paymaster ===
+
   const paymaster = createPaymasterClient({
     chain: monadTestnet,
     transport: http(
@@ -85,7 +89,7 @@ export async function sendCalls(ctx, { to, data, value = 0n }) {
   const hash = await bundler.sendUserOperation({
     account: smartAccount,
     calls: [{ to, data, value }],
-    paymaster, // <— ВАЖНО: добавлено
+    paymaster, 
   });
   return { hash };
 }
