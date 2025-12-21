f856066749e54707ce24e87076cd1f3cb0c0c4d4
vikions <plambir1994@mail.ru>
2025-12-21 22:49:29 +0700

    Fix: Remove MiniAppProvider that was overriding baseAccount config

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import MiniAppProvider from './MiniAppProvider';
-import { tryReadyMiniApp } from './miniapp';
 
 const root = document.getElementById('root');
 
 ReactDOM.createRoot(root).render(
-  <MiniAppProvider>
-    <App />
-  </MiniAppProvider>
+  <App />
 );
-
-
-tryReadyMiniApp();
